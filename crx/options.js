23254094008f49437d34b7b2cdbd6b9e34ad0315
Xianming Zhong <chinesedfan@qq.com>
2018-03-14 14:34:16 +0800

    prepare options page

---
+++ crx/options.js
@@ -0,0 +1,29 @@
+var eles = document.querySelectorAll('.item [data-key]');
+eles = Array.prototype.slice.call(eles);
+
+// load
+eles.forEach(function(el) {
+    var key = el.getAttribute('data-key');
+    var value = localStorage.getItem(key) || '';
+    if (value != el.getAttribute('placeholder')) {
+        el.value = value;
+    }
+});
+
+// save
+var eleBtn = document.querySelector('.btn');
+var eleMsg = document.querySelector('.msg');
+eleBtn.addEventListener('click', function() {
+    eles.forEach(function(el) {
+        localStorage.setItem(el.getAttribute('data-key'), el.value || el.getAttribute('placeholder'));
+    });
+    eleMsg.innerHTML = '...done!';
+});
+eles.forEach(function(el) {
+    el.addEventListener('input', function() {
+        eleMsg.innerHTML = 'Hmm...';
+    });
+    el.addEventListener('focus', function() {
+        this.select();
+    });
+});
