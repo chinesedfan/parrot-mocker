5c20639c1bbcbdade117649aa226702697941653
Xianming Zhong <chinesedfan@qq.com>
2018-04-07 00:54:09 +0800

    passed basic cases with jsdom

--- test/crx/popup.test.js
+++ test/crx/popup.test.js
@@ -1,20 +1,53 @@
+const fs = require('fs');
+const path = require('path');
 const chrome = require('sinon-chrome');
-const assert = require('sinon').assert;
+const { assert, stub } = require('sinon');
+const { JSDOM } = require('jsdom');
 
 describe('background.js', function() {
+    let script;
+    let window, eleInput, eleMsg, eleBtn;
+
     beforeAll(function() {
-        global.chrome = chrome;
+        script = fs.readFileSync(path.resolve(__dirname, '../../crx/popup.js')).toString();
+
+        chrome.tabs.query.callsArgWith(1, [{id: 123}]);
     });
 
     beforeEach(function() {
-        chrome.flush();
-        jest.resetModules();
+        // TODO: chrome.flush();
 
-        require('../../crx/popup');
+        return JSDOM.fromFile(path.resolve(__dirname, '../../crx/popup.html'), {
+            runScripts: 'outside-only'
+        }).then(function(dom) {
+            window = dom.window;
+            window.chrome = chrome;
+
+            const document = window.document;
+            document.addEventListener = stub().callsArg(1);
+
+            eleInput = document.getElementById('mockserver');
+            eleMsg = document.getElementsByClassName('msg')[0];
+            eleBtn = document.getElementsByClassName('btn')[0];
+        });
     });
 
+    it('should call required functions', function() {
+        window.eval(script);
+
+        assert.calledOnce(chrome.tabs.query);
+        assert.calledOnce(chrome.tabs.sendMessage);
+    });
     describe('render', function() {
         it('should render correctly if is locked', function() {
+            chrome.tabs.sendMessage.callsArgWith(2, {
+                locked: true
+            });
+            window.eval(script);
+
+            expect(eleMsg.innerHTML).toEqual('Don\'t use at the mocker website!');
+            expect(eleBtn.innerHTML).toEqual('Unable to Mock');
+            expect(eleBtn.className).toEqual('btn locked');
         });
         it('should render correctly if is enabled and has client id', function() {
         });
