8865514a5c7992efebdfede846a1016810512d34
Xianming Zhong <chinesedfan@qq.com>
2018-04-12 23:02:28 +0800

    ignore some files for coverage

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
+    coveragePathIgnorePatterns: [
+        '<rootDir>/src/common/cookies.js',
+        '<rootDir>/src/wrapper/',
+        '<rootDir>/node_modules/'
+    ],
     testEnvironment: 'node'
 };
