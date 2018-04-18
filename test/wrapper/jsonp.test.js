65744b2409e5b100e8a692e304c6db1955e300f0
Xianming Zhong <chinesedfan@qq.com>
2018-04-18 19:52:28 +0800

    test side effects

--- test/wrapper/jsonp.test.js
+++ test/wrapper/jsonp.test.js
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 
+const { JSDOM } = require('jsdom');
 const jsonp = require('../../src/wrapper/jsonp.js');
 
 let wrapUrl;
@@ -14,6 +15,48 @@ describe('jsonp', function() {
         wrapUrl.mockClear();
     });
 
+    describe('no side effects', function() {
+        it('should ignore non-script', function() {
+            const script = document.createElement('img');
+            script.src = 'xx.com?callback=jsonpcb';
+
+            expect(wrapUrl).toHaveBeenCalledTimes(0);
+            expect(script.src).toEqual('xx.com?callback=jsonpcb');
+        });
+        it('should ignore non-src', function() {
+            const script = document.createElement('script');
+            script.type = 'xx.com?callback=jsonpcb';
+
+            expect(wrapUrl).toHaveBeenCalledTimes(0);
+            expect(script.type).toEqual('xx.com?callback=jsonpcb');
+        });
+        it('should ignore non-jsonp src', function() {
+            const script = document.createElement('script');
+            script.src = 'xx.com';
+
+            expect(wrapUrl).toHaveBeenCalledTimes(0);
+            expect(script.src).toEqual('xx.com');
+        });
+        it('should not break src/getAttribute', function() {
+            const dom = new JSDOM(``, {
+                url: 'https://xx.com/otherpath',
+                runScripts: 'outside-only'
+            });
+            const win = dom.window;
+            // prepare in sub-jsdom
+            win.wrapUrl = wrapUrl;
+            win.eval(`
+                (${jsonp.init.toString()})(wrapUrl);
+                window.script = document.createElement('script');
+                script.src = 'path';
+            `);
+
+            const script = win.script;
+            expect(wrapUrl).toHaveBeenCalledTimes(0);
+            expect(script.src).toEqual('https://xx.com/path');
+            expect(script.getAttribute('src')).toEqual('path');
+        });
+    });
     it('should call wrapUrl in script.src.setter', function() {
         const script = document.createElement('script');
         script.src = 'xx.com?callback=jsonpcb';
